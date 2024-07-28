51987d7a1f2d88c9a17faf367cf5054847ebcaaa
lenya1567 <lenya@leshak.ru>
2024-07-29 00:29:46 +0300

    update user page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Skeleton, TextField } from "@mui/material";
+import { Alert, Button, Container, Skeleton, TextField } from "@mui/material";
 import NavBar from "../components/Navbar";
 import { UserApi } from "../api/UserApi";
 import React from "react";
@@ -62,34 +62,37 @@ function UserPage() {
 
     return <>
         <NavBar forUpdate={update} />
-        <div style={{ padding: 16, paddingTop: 0, maxWidth: 500 }}>
-            <h2>Личный кабинет</h2>
-            { user.successUpdate && <Alert style={{ marginBottom: 12 }}icon={<CheckCircleOutline fontSize="inherit" />} severity="success">
-                Данные успешно сохранены!
-            </Alert> 
-            }
-            { user.login == true ?
-            <>
-                <div>
-                    <TextField onChange={(ev) => updateUserField("name", ev)} defaultValue={user.userData.name} style={{ marginBottom: 12 }} label="Ваше имя:" fullWidth/>
-                    <TextField defaultValue={user.userData.login} style={{ marginBottom: 12 }} disabled label="Ваш логин:" fullWidth variant="filled"/>
-                    <TextField defaultValue={user.userData.status} style={{ marginBottom: 12 }} disabled label="Вид пользователя:" fullWidth variant="filled"/>
-                </div>
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <Button onClick={logoutUser}>Выйти</Button>
-                    <Button disabled={!user.changed} onClick={updateUser}>Сохранить изменения</Button>
-                </div>
-            </>
-            : <>
-                <div>
-                    <Skeleton width={"100%"} height={56} style={{ marginBottom: 12 }}/>
-                    <Skeleton width={"100%"} height={56} style={{ marginBottom: 12 }}/>
-                    <Skeleton width={"100%"} height={56} style={{ marginBottom: 12 }}/>
-                </div>
-            </>
-                
-            }
-        </div>
+        <Container maxWidth="xl">
+            <div style={{ maxWidth: 500 }}>
+                <h2>Личный кабинет</h2>
+                {user.successUpdate && <Alert style={{ marginBottom: 12 }} icon={<CheckCircleOutline fontSize="inherit" />} severity="success">
+                    Данные успешно сохранены!
+                </Alert>
+                }
+                {user.login == true ?
+                    <>
+                        <div>
+                            <TextField onChange={(ev) => updateUserField("name", ev)} defaultValue={user.userData.name} style={{ marginBottom: 12 }} label="Ваше имя:" fullWidth />
+                            <TextField defaultValue={user.userData.login} style={{ marginBottom: 12 }} disabled label="Ваш логин:" fullWidth variant="filled" />
+                            <TextField defaultValue={user.userData.status} style={{ marginBottom: 12 }} disabled label="Вид пользователя:" fullWidth variant="filled" />
+                        </div>
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                            <Button onClick={logoutUser}>Выйти</Button>
+                            <Button disabled={!user.changed} onClick={updateUser}>Сохранить изменения</Button>
+                        </div>
+                    </>
+                    : <>
+                        <div>
+                            <Skeleton width={"100%"} style={{ height: 56 }} />
+                            <Skeleton width={"100%"} style={{ height: 56 }} />
+                            <Skeleton width={"100%"} style={{ height: 56 }} />
+                        </div>
+                    </>
+
+                }
+            </div>
+        </Container>
+
     </>
 }
 
